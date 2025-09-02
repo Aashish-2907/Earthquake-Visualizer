9a5916b4b6f835c332d397c7c72ef35ce37ad76f
Aashish Raj <aashishraj2907.com>
2025-09-02 23:37:50 +0530

    Sort func added

--- Earthquake-Visualiser/src/QuakeList.jsx
+++ Earthquake-Visualiser/src/QuakeList.jsx
@@ -2,12 +2,16 @@ import React,{useState,useEffect} from "react";
 import axios from "axios"
 import {MapContainer,Marker,TileLayer,Popup} from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { FaSortAmountDown } from "react-icons/fa";
 // import { MapBox } from "./MapBox";
 
 export default function QuakeList (){
     const[quakes,setQuakes]=useState([])
     const [error,setError]=useState("");
     const [loading,setLoading]=useState(true)
+    const [showData,setshowData]=useState(false);
+    const [asc,setAsc]=useState(false);
 
     const fetchQuakes = async ()=>{
         try {
@@ -27,21 +31,52 @@ export default function QuakeList (){
 
   
     return(
-        <div>
-            {/* <h2>Test Earthquake Data:</h2>
-            {loading && <p>Loading...</p>}
+            <div className="flex">
+                 <div className="w-1/2 p-4">
+                    {loading && <AiOutlineLoading3Quarters />}
+                    {error && <p className="text-red-500">{error}</p>}
+                    <button  onClick={()=>{setshowData(!showData);}} className="bg-blue-500 text-white px-6 py-4 rounded cursor-pointer">
+                        {showData ? "Hide Data" : "Show Data"}
+                    </button>
+                    {showData && (
+                    <button 
+                         onClick={() => setAsc(!asc)} 
+                         className="ml-4 bg-green-500 text-white px-4 py-2 rounded flex items-center gap-2"
+                    >
+                    <FaSortAmountDown />
+                     {asc ? "Sort Desc" : "Sort Asc"}
+                    </button>
+                    
+                    )}
+                    <ul className="space-y-4 mt-4">
+                    {showData && 
+                    [...quakes]
+                     .sort((a, b) => asc ? a.properties.mag - b.properties.mag : b.properties.mag - a.properties.mag)
+                    .map((quake) => (
+                    <li key={quake.id}>
+                    {quake.properties.place} - 
+                    <span className="mx-8 font-bold">Magnitude:</span> 
+                    {quake.properties.mag}
+                     </li>
+      ))
+  }
+</ul>
+                     <ul className="space-y-4">
+                        { showData && quakes.map((quake)=>(
+                            <li key={quake.id} >
+                                {quake.properties.place}- <span className="mx-8 font-bold">Magnitude:</span>{quake.properties.mag}
+                                {/* {showData && <FaSortAmountDown/>} */}
+                            </li>
+                            
+                    ))}
+                      
+                     </ul>
+         
+            </div>
+            {loading && <AiOutlineLoading3Quarters />}
             {error && <p>{error}</p>}
-            <ul>
-                { quakes.map((quake)=>(
-                    <li key={quake.id}>{quake.properties.place}-{quake.properties.time}</li>
-                ))
-                }
-            </ul> */}
-
-            {loading && <p>Loading...</p>}
-            {error && <p>{error}</p>}
-            <ul>
-                <MapContainer center={[20, 0]} zoom={2} >
+            <div className="w-1/2 h-screen">
+                <MapContainer center={[20, 0]} zoom={2} className="h-full w-full">
                 <TileLayer
                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -57,10 +92,10 @@ export default function QuakeList (){
                         </Marker>
                     )     
                 })}         
-        </MapContainer>                 
-            </ul>                        
-        </div>
-    )
+        </MapContainer> 
+            </div>
+        </div>                       
+    );
 }
 
 
