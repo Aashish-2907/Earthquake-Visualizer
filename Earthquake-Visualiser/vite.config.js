902bf58538b9a45ec3a1711ef52c667fbcbadb74
Aashish Raj <aashishraj2907.com>
2025-09-03 14:07:52 +0530

    Tailwind debugged

--- Earthquake-Visualiser/vite.config.js
+++ Earthquake-Visualiser/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  css: {
-    // Disable lightningcss, fallback to PostCSS
-    postcss: {}
-  }
+  plugins: [react()],
+  // css: {
+  //   // Disable lightningcss, fallback to PostCSS
+  //   postcss: {}
+  // }
 })
